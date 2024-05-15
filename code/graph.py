f079d47f14d3fbebaf927586db78f3d858c3221a
sss <>
2024-05-15 11:54:58 +0800

    添加边权重

--- code/graph.py
+++ code/graph.py
@@ -1,26 +1,29 @@
 import re
 
+
 class Graph:
     def __init__(self) -> None:
-        self.node = {}
+        self.graph_dict = {}
 
     def __str__(self) -> str:
         graph_str = ''
-        for item in self.node.items():
-            graph_str += item[0] + ' -> '
-            for end in item[1]:
-                graph_str += end + ' '
+        for (fv, tv) in self.graph_dict.items():
+            graph_str += fv + ' -> '
+            for (name, weight) in tv.items():
+                graph_str += f'{name}: {weight}, '
             graph_str += '\n'
         return graph_str
 
     def add_vertex(self, name: str):
-        if name not in self.node.keys():
-            self.node[name] = []
+        if name not in self.graph_dict.keys():
+            self.graph_dict[name] = {}
 
     def add_edge(self, from_name: str, to_name: str):
         self.add_vertex(from_name)
         self.add_vertex(to_name)
-        self.node[from_name].append(to_name)
+        if to_name not in self.graph_dict[from_name].keys():
+            self.graph_dict[from_name][to_name] = 0
+        self.graph_dict[from_name][to_name] += 1
 
 
 class WordGraph:
@@ -31,6 +34,9 @@ class WordGraph:
         for i in range(len(words) - 1):
             self.graph.add_edge(words[i], words[i+1])
         print(self.graph)
+
+    def __str__(self) -> str:
+        return self.graph.__str__()
 
 
 
